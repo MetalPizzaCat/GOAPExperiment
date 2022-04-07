280f40f89d0a2db012b9e9a32bef3805e14c0c84
MetalCat <36876492+MetalPizzaCat@users.noreply.github.com>
2022-04-07 16:04:58 +1000

    update

--- Experiment/Experiment.cpp
+++ Experiment/Experiment.cpp
@@ -14,6 +14,14 @@
 #define PRINT_LINE(color,text) std::cout << color << text << termcolor::reset << std::endl
 #define RESET_COLOR() std::cout << termcolor::reset << std::endl
 #define SCREEN_CLEAR() cout << "\033[2J\033[1;1H"
+#define HUMAN_SUBTRACT_OR_ADD_TASK(resourceName,task) if (_world.##resourceName##Count < t->Requirments.resourceName)\
+				{\
+				AddTask(_taskManager->Get(#task));\
+				}\
+else\
+				{\
+				_world.##resourceName##Count -= t->Requirments.resourceName;\
+				}
 using namespace std;
 
 enum ETools
@@ -35,18 +43,15 @@ struct Task
 {
 	const char* Name;
 	Requirement Requirments;
-	vector<Requirement> Returns;
+	Requirement Returns;
 	//simulation of doing something by means of waiting
 	//time in game ticks
 	int ExecutionTime;
 
 	Task(const char* name, int execTime) :
 		Name(name), ExecutionTime(execTime) {}
-	Task(const char* name, Requirement && requirments, int execTime) :
-		Name(name), ExecutionTime(execTime),Requirments(requirments) {}
-
-	Task(const char* name, Requirement requirments, int execTime) :
-		Name(name), ExecutionTime(execTime), Requirments(requirments) {}
+	Task(const char* name, Requirement requirments, int execTime, Requirement returns = {}) :
+		Name(name), ExecutionTime(execTime), Requirments(requirments),Returns(returns) {}
 };
 //struct that contains all of the world's resources
 struct World
@@ -61,12 +66,12 @@ class TaskManager
 private:
 	vector<Task*> _tasks;
 public:
-	bool Add(const char* name, int executionTime,Requirement requirement)
+	bool Add(Task*task)
 	{
-		vector<Task*>::iterator it = std::find_if(_tasks.begin(), _tasks.end(), [name](Task* task) {return task->Name == name; });
+		vector<Task*>::iterator it = std::find_if(_tasks.begin(), _tasks.end(), [task](Task* task) {return task->Name == task->Name; });
 		if (it == _tasks.end())
 		{
-			_tasks.push_back(new Task(name, requirement, executionTime));
+			_tasks.push_back(task);
 			return true;
 		}
 		return false;
@@ -124,6 +129,8 @@ public:
 	const char* Name;
 	void AddTask(Task const* task)
 	{
+		if (!task)
+			return;
 		if (task->Requirments.Log > 0)
 		{
 			if (_world.LogCount >= task->Requirments.Log)
@@ -165,7 +172,16 @@ public:
 			if (_currentTaskExecutionCounter  >= (*_tasks.begin())->ExecutionTime)
 			{
 				_currentTaskExecutionCounter = 0;
+				Task const* t = (*_tasks.begin());
+
 				_tasks.erase(_tasks.begin());
+				HUMAN_SUBTRACT_OR_ADD_TASK(Food,get_food);
+				HUMAN_SUBTRACT_OR_ADD_TASK(Iron, get_iron);
+				HUMAN_SUBTRACT_OR_ADD_TASK(Log, get_logs);
+
+				_world.FoodCount += t->Returns.Food;
+				_world.IronCount += t->Returns.Iron;
+				_world.LogCount += t->Returns.Log;
 			}
 		}
 	}
@@ -181,10 +197,10 @@ int main()
 		Human(world,"Vasya",manager.get()),
 		Human(world,"Elena",manager.get())
 	};
-	manager->Add("make_plank", 20, { 20,0,0,ETools::ET_Axe });
-	manager->Add("cut_trees", 20, { 0,0,0,ETools::ET_Axe });
-	manager->Add("get_logs", 20, { 0,0,0,ETools::ET_Axe });
-	
+	manager->Add(new Task("make_plank", { 20,0,0,ETools::ET_Axe }, 20));
+	manager->Add(new Task("cut_trees", { 0,0,0,ETools::ET_Axe }, 20));
+	manager->Add(new Task("get_logs", { 0,0,0,ETools::ET_Axe }, 5));
+
 	people[0].AddTask(manager->operator[]("make_plank"));
 	people[1].AddTask(manager->operator[]("make_plank"));
 	while (true)
